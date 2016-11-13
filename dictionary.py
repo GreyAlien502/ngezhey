fab7563909ceefe1b996711b79583ab1bc86511c
GreyAlien502 <GreyAlien502@gmail.com>
2016-11-13 00:34:50 -0500

    added reverse search capability

--- dictionary.py
+++ dictionary.py
@@ -12,22 +12,27 @@ def translate(word):
 			return entry
 	print('ERROR: '+word+' not found')
 
-def search(string, mode="start", ):
-	if mode == "start":
-		match = lambda x,y: y['english'].startswith(x)
-	elif mode == "exact":
-		match = lambda x,y: x==y['english']
-	elif mode == "contains":
-		match = lambda x,y: x in (y['english']+' '+' '.join(y['notes']))
-
+def search(string, mode="start", to='eng'):
 	matches = []
-	for entry in dictionary:
-		matchingTranslations = []
-		for translation in entry['translations']:
-			if match(string,translation):
-				matchingTranslations.append(translation)
-		nuvoentry = entry.copy()
-		nuvoentry['translations'] = matchingTranslations
-		if nuvoentry['translations']!=[]:
-			matches.append(nuvoentry)
+	if mode == "start":
+		match = lambda x,y: y[0].startswith(x)
+	elif mode == "exact":
+		match = lambda x,y: x==y[0]
+	elif mode == "contains":
+		match = lambda x,y: x in (' '.join(y))
+
+	if to=='eng':
+		for entry in dictionary:
+			if match(string,[entry['original']]):
+				matches.append(entry)
+	if to=='nge':
+		for entry in dictionary:
+			matchingTranslations = []
+			for translation in entry['translations']:
+				if match(string,[translation['english'],' '.join(translation['notes'])]):
+					matchingTranslations.append(translation)
+			nuvoentry = entry.copy()
+			nuvoentry['translations'] = matchingTranslations
+			if nuvoentry['translations']!=[]:
+				matches.append(nuvoentry)
 	return matches
